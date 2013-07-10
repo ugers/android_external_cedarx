2b667b9608bf16a8894ad77d059bd917a3ddf9e1
ugers <yugers@gmail.com>
2013-07-11 00:35:09 +0600

    Update virtual_hwcomposer.cpp

--- CedarXAndroid/IceCreamSandwich/virtual_hwcomposer.cpp
+++ CedarXAndroid/IceCreamSandwich/virtual_hwcomposer.cpp
@@ -24,7 +24,41 @@
 //#elif (CEDARX_ANDROID_VERSION == 7)
 namespace android {
 
-#if defined(__CHIP_VERSION_F33)
+#if (defined(__CHIP_VERSION_F23) || defined(__CHIP_VERSION_F51))
+int convertlibhwclayerpara_NativeRendererVirtual2Arch(libhwclayerpara_t *pDes, Virtuallibhwclayerpara *pSrc)
+{
+    memset(pDes, 0, sizeof(libhwclayerpara_t));
+    pDes->number            = pSrc->number;
+    pDes->bProgressiveSrc   = pSrc->bProgressiveSrc;
+    pDes->bTopFieldFirst    = pSrc->bTopFieldFirst;
+    pDes->flag_addr         = pSrc->flag_addr;
+    pDes->flag_stride       = pSrc->flag_stride;    
+    pDes->maf_valid         = pSrc->maf_valid;      
+    pDes->pre_frame_valid   = pSrc->pre_frame_valid;
+    if(pSrc->source3dMode == CEDARV_3D_MODE_DOUBLE_STREAM && pSrc->displayMode == CEDARX_DISPLAY_3D_MODE_3D)
+    {
+        pDes->top_y         = pSrc->top_y;
+		pDes->top_c         = pSrc->top_u;
+		pDes->bottom_y      = pSrc->top_y2;
+		pDes->bottom_c      = pSrc->top_u2;
+    }
+    else if(pSrc->displayMode == CEDARX_DISPLAY_3D_MODE_ANAGLAGH)
+	{
+        pDes->top_y         = pSrc->top_u2;
+		pDes->top_c         = pSrc->top_y2;
+		pDes->bottom_y      = pSrc->top_v2;
+		pDes->bottom_c      = 0;
+    }
+    else
+    {
+        pDes->top_y             = pSrc->top_y;
+        pDes->top_c             = pSrc->top_u;
+        pDes->bottom_y          = 0;
+        pDes->bottom_c          = 0;
+    }
+    return OK;
+}
+#elif defined(__CHIP_VERSION_F33)
 int convertlibhwclayerpara_NativeRendererVirtual2Arch(libhwclayerpara_t *pDes, Virtuallibhwclayerpara *pSrc)
 {
     memset(pDes, 0, sizeof(libhwclayerpara_t));
@@ -39,7 +73,7 @@ int convertlibhwclayerpara_NativeRendererVirtual2Arch(libhwclayerpara_t *pDes, V
     {
         if(pSrc->source3dMode == CEDARV_3D_MODE_DOUBLE_STREAM && pSrc->displayMode == CEDARX_DISPLAY_3D_MODE_3D)
         {
-            pDes->addr[0]   	  = pSrc->top_y;
+            pDes->addr[0] 		  = pSrc->top_y;
     		pDes->addr[1] 		  = pSrc->top_u;
     		pDes->addr[2] 		  = 0;
     		pDes->addr_3d_right[0] = pSrc->top_y2;
@@ -94,6 +128,9 @@ int convertlibhwclayerpara_SoftwareRendererVirtual2Arch(libhwclayerpara_t *pDes,
 	pDes->addr_3d_right[2]	 	= 0;
     return OK;
 }
+#else
+    #error "Unknown chip type!"
 #endif
 
 }; // namespace android
+
